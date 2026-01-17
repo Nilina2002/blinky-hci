356b854561d8b4a906bc4921ea7edeba9e7e4ee8
Amarathunga A.A.N.N <nilina610@gmail.com>
2026-01-17 11:30:57 +0530

    Add voice command functionality to control ESP32-S3 LED

---
+++ v1/voice-command.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+import requests
+
+ESP32_IP = "192.168.8.106"
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+print("Listening for 'hello podda'...")
+
+while True:
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio).lower()
+        print("Heard:", text)
+
+        if "hello" in text:
+            requests.get(f"http://{ESP32_IP}/on")
+            print("LED ON (wireless)")
+
+    except:
+        pass
